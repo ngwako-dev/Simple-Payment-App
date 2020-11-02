67c50354a896fcea291529080febca58364d6388
Ngwako Gift Moshobane <ngmoshobane@gmail.com>
2020-11-02 08:10:56 +0200

    change from linear to constraints layout

--- app/src/main/java/android/example/com/simplepaymentapp/peopleFragment.kt
+++ app/src/main/java/android/example/com/simplepaymentapp/peopleFragment.kt
@@ -1,59 +1,57 @@
 package android.example.com.simplepaymentapp
 
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [peopleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class peopleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var listView: ListView
+    private lateinit var navController: NavController
 
     override fun onCreateView(
+
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_people, container, false)
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment peopleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            peopleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private val listItems = listOf("Albatruna","Alligamish","Buffaros","Chimadger","Chimpow","Demonkey","Doomling","Fiepecker","Gothyte","parsnipe")
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listView = view.findViewById(R.id.myListview)
+        navController= Navigation.findNavController(view)
+
+
+
+        val adapter =ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,listItems)
+        listView.adapter=adapter
+
+        listView.setOnItemClickListener{parent, view, position, id ->
+
+
+            //  var listItems =parent.getItemAtPosition(position) as String
+            navController.navigate(R.id.action_peopleFragment_to_payFragment)
+
+        }
+
     }
+
+
 }
+
